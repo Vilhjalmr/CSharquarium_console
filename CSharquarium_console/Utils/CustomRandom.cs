1ae4efa247f651a5dd137db76c1cf4beac71afd1
Vilhjalmr <guillaume.danneau@gmail.com>
2016-06-15 16:32:16 +0200

    Made CustomRandom.GetRandom both simpler and better

--- CSharquarium_console/Utils/CustomRandom.cs
+++ CSharquarium_console/Utils/CustomRandom.cs
@@ -8,29 +8,13 @@ namespace CSharquarium_console.Utils
 {
     public static class CustomRandom
     {
-        public static Random r = new Random();
-
         public static int GetRandom(int v)
         {
             int result = Math.Abs(Guid.NewGuid().GetHashCode());
 
-            while (result >= v)
-            {
-                // TODO: try catch pour éviter que result %/ x = 0 remplacer 10 par random entre 0 et v
-                // TODO: make sure this actually works.
-                // TODO: put used line numbers in static array. If nbr already picked, pick again
-                int i = r.Next(v);
-                try
-                {
-                    result %= i;
-                }
-                catch (DivideByZeroException)
-                {
-                    result = GetRandom(v);
-                }
-            }
+            result %= v;
 
-            return Math.Abs(result);
+            return result;
         }
     }
 
